52b393b263b6db5a8a732148fcac859c5806fd38
Minho Menezes <minhotmog@gmail.com>
2018-11-03 17:46:58 -0300

    Adding the multi-scroll feature and fixing some bugs

--- js/script.js
+++ js/script.js
@@ -1,143 +1,158 @@
-// Funções do Scrollama
-// generic window resize listener event
-function handleResize() {
-	// 1. update height of step elements
-	var stepHeight = Math.floor(window.innerHeight * 0.90);
-	step.style('height', stepHeight + 'px');
+function init(selector) {
+	// using d3 for convenience
+	var container = d3.selectAll(selector);
 
-	// 2. update width/height of graphic element
-	var bodyWidth = d3.select('body').node().offsetWidth;
+	var graphic = container.select('.scroll__graphic');
+	var text = container.select('.scroll__text');
+	var step = text.selectAll('.step');
 
-	var graphicMargin = 16 * 4;
-	var textWidth = text.node().offsetWidth;
-	var graphicWidth = container.node().offsetWidth - textWidth;
-	var graphicHeight = Math.floor(window.innerHeight)
-	var graphicMarginTop = Math.floor(graphicHeight / 2)
+	// initialize the scrollama
+	var scroller = scrollama();
 
-	var scroll_graphics = $(".scroll__graphic__img")
-	for (var i = scroll_graphics.length - 1; i >= 0; i--) {
-		$(scroll_graphics[i]).hide()
-	}
+	// initially hide everything
+	container.selectAll(".scroll__graphic__img")._groups
+		 .forEach(function(d, i) {
+		  $(d).hide()
+	   })
 
-	var scroll_text = $(".step > p")
-	for (var i = scroll_text.length - 1; i >= 0; i--) {
-		$(scroll_text[i]).hide()
-	}
-
-	graphic
-		.style('width', graphicWidth + 'px')
-		.style('height', graphicHeight + 'px')
-
-	$("#intro").css("height", Math.floor(window.innerHeight/2));
-
-	// 3. tell scrollama to update new element dimensions
-	scroller.resize();
-}
-
-// scrollama event handlers
-function handleStepEnter(response) {
-	// response = { element, direction, index }
-	console.log("Entering: " + response.direction + " " + response.index)
-
-	// add color to current step only
-	step.classed('is-active', function (d, i) {
-		return i === response.index;
+	step.selectAll("p")._groups
+	   .forEach(function(d, i) {
+		$(d).hide()
 	})
 
-	// # UPDATE GRAPHIC BASED ON STEP #
-	var graph = $(".scroll__graphic");
-	var text = $(".scroll__text");
+	// scrollama event handlers
+	function handleStepEnter(response) {
+		// response = { element, direction, index }
+		// console.log(response.element)
+		// console.log("Entering: " + response.direction + " " + response.index)
 
-	// Get the working text panels for the current step
-	var actText = $("#step_"+(response.index)+ " > p");
-	var prevText = (response.direction == 'down') ? 
-					$("#step_"+(Math.max(0,response.index-1))+ " > p") 
-					: $("#step_"+(response.index+1)+ " > p");
+		// add color to current step only
+		step.classed('is-active', function (d, i) {
+			return i === response.index;
+		})
 
-	// Get the working graphics for the current step
-	var actGraph = $("#scroll__graphic__img_"+(response.index));
-	var prevGraph = (response.direction == 'down') ? 
-					 $("#scroll__graphic__img_"+(Math.max(0,response.index-1))) 
-					 : $("#scroll__graphic__img_"+(response.index+1));
+		// # UPDATE GRAPHIC BASED ON STEP #
+		var graph = $(".scroll__graphic");
+		var textbox = $(".scroll__text");
 
-	if(actGraph.hasClass("left_panel")) {
-		graph.animate({left: "0%"})
-		text.animate({left: "65%"})
+		// Get the working text panels for the current step
+		var actText = $("#step_"+(response.index)+ " > p");
+		var prevText = (response.direction == 'down') ? 
+						$("#step_"+(Math.max(0,response.index-1))+ " > p") 
+						: $("#step_"+(response.index+1)+ " > p");
+
+		// Get the working graphics for the current step
+		var actGraph = $("#scroll__graphic__img_"+(response.index));
+		var prevGraph = (response.direction == 'down') ? 
+						 $("#scroll__graphic__img_"+(Math.max(0,response.index-1))) 
+						 : $("#scroll__graphic__img_"+(response.index+1));
+
+
+		if(actGraph.hasClass("left_panel")) {
+			graph.stop().animate({left: "0%"})
+			textbox.stop().animate({left: "65%"})
+		}
+		else if(actGraph.hasClass("right_panel")) {
+			graph.stop().animate({left: "35%"})
+			textbox.stop().animate({left: "0%"})
+		}
+
+		// Fade-out the previous graphic and fade-in the actual one
+		$(prevGraph[0]).stop().fadeOut(500, function() {
+			$(actGraph[0]).fadeIn(500);
+		})
+
+		for (var i = prevText.length - 1; i >= 0; i--) {
+			$(prevText[i]).stop().fadeOut(500)
+		}
+		for (var i = actText.length - 1; i >= 0; i--) {
+			$(actText[i]).fadeIn(1500)
+		}
 	}
-	else if(actGraph.hasClass("right_panel")) {
-		graph.animate({left: "35%"})
-		text.animate({left: "0%"})
-	}
-	else if(actGraph.hasClass("center_panel")) {
+
+	function handleStepExit(response) {
+		// response = { element, direction, index }
+		console.log("Leaving: " + response.direction + " " + response.index)
+
+		// update graphic based on step
+		var prevGraph = (response.direction == 'down') ? 
+						 $("#scroll__graphic__img_"+(Math.max(0,response.index-1))) 
+						 : $("#scroll__graphic__img_"+(response.index+1));
+
+		prevGraph.stop().fadeOut(100);
 
 	}
 
-	// Fade-out the previous graphic and fade-in the actual one
-	prevGraph.stop().fadeOut(500, function() {
-		actGraph.fadeIn(500);
-	})
+	function handleContainerEnter(response) {
+		// response = { direction }
 
-	for (var i = prevText.length - 1; i >= 0; i--) {
-		$(prevText[i]).stop().fadeOut(500)
+		// old school
+		// sticky the graphic
+		// graphic.classed('is-fixed', true);
+		// graphic.classed('is-bottom', false);
 	}
-	for (var i = actText.length - 1; i >= 0; i--) {
-		$(actText[i]).fadeIn(1500)
+
+	function handleContainerExit(response) {
+		// response = { direction }
+
+		// old school
+		// un-sticky the graphic, and pin to top/bottom of container
+		// graphic.classed('is-fixed', false);
+		// graphic.classed('is-bottom', response.direction === 'down');
 	}
-}
 
-// scrollama event handlers
-function handleStepExit(response) {
-	// response = { element, direction, index }
-	// console.log("Leaving: " + response.direction + " " + response.index)
+	(function setupStickyfill() {
+		d3.selectAll(".sticky").each(function() {
+			  Stickyfill.add(this);
+		});
+	})();
 
-	// update graphic based on step
-	var prevGraph = (response.direction == 'down') ? 
-					 $("#scroll__graphic__img_"+(Math.max(0,response.index-1))) 
-					 : $("#scroll__graphic__img_"+(response.index+1));
-
-	prevGraph.stop().fadeOut(100);
-
-}
-
-function handleContainerEnter(response) {
-	// response = { direction }
-
-	// old school
-	// sticky the graphic
-	graphic.classed('is-fixed', true);
-	graphic.classed('is-bottom', false);
-}
-
-function handleContainerExit(response) {
-	// response = { direction }
-
-	// old school
-	// un-sticky the graphic, and pin to top/bottom of container
-	graphic.classed('is-fixed', false);
-	graphic.classed('is-bottom', response.direction === 'down');
-}
-
-function init() {
 	// 1. force a resize on load to ensure proper dimensions are sent to scrollama
-	handleResize();
+	// generic window resize listener event
+	function handleResize() {
+		// 1. update height of step elements
+		var stepHeight = Math.floor(window.innerHeight * 0.90);
+		step.style("height", stepHeight + "px");
+
+		// 2. update width/height of graphic element
+		var bodyWidth = d3.select("body").node().offsetWidth;
+
+		var graphicMargin = 16 * 4;
+		var textWidth = text.node().offsetWidth;
+		var graphicWidth = container.node().offsetWidth - textWidth;
+		var graphicHeight = Math.floor(window.innerHeight);
+		var graphicMarginTop = Math.floor(graphicHeight / 2);
+
+		graphic
+			.style("width", graphicWidth + "px")
+			.style("height", graphicHeight + "px")
+			.style("top", 0);
+
+		$("#intro").css("height", Math.floor(window.innerHeight/2));
+	
+		// 3. tell scrollama to update new element dimensions
+		scroller.resize();
+	}
 
 	// 2. setup the scroller passing options
 	// this will also initialize trigger observations
-
 	// 3. bind scrollama event handlers (this can be chained like below)
-	scroller.setup({
-		container: '#scroll',
-		graphic: '.scroll__graphic',
-		text: '.scroll__text',
-		step: '.scroll__text .step',
-		debug: false,
-		offset: 0.33,
-	})
+	scroller
+		.setup({
+		  container: selector,
+		  graphic: ".scroll__graphic",
+		  text: ".scroll__text",
+		  step: ".scroll__text .step",
+		  debug: false,
+		  offset: 0.5
+		})
 		.onStepEnter(handleStepEnter)
 		.onStepExit(handleStepExit)
 		.onContainerEnter(handleContainerEnter)
 		.onContainerExit(handleContainerExit);
 
 	// setup resize event
-	window.addEventListener('resize', handleResize);
+	window.addEventListener("resize", handleResize);
+	handleResize();
 }
+
